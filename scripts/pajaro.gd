640aa20fdb9e557bf6acd96727460b687548cc13
Fabian <fabianperez592@gmail.com>
2019-12-21 20:56:01 -0300

    completado 02

---
+++ scripts/pajaro.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+func _ready():
+	pass
+
+func _process(delta):
+#	print(rad2deg(rotation))
+	if rad2deg(rotation) < -30:
+		set_rotation(deg2rad(-30))
+		set_angular_velocity(0)
+		
+	if linear_velocity.y > 0:
+		set_angular_velocity(3)
+	pass
+	
+func aletear():
+	set_linear_velocity(Vector2(linear_velocity.x, -150))
+	set_angular_velocity(-3)
+	pass
+	
+func _input(event):
+	if Input.is_action_pressed("aletear"):
+		aletear()
+	pass
