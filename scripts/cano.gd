56ee043444eb3ae2b28aefdbf84b1d43b12c471a
Fabian <fabianperez592@gmail.com>
2019-12-23 16:35:22 -0300

    fix: ahora sí spawnea caños infinitamente

--- scripts/cano.gd
+++ scripts/cano.gd
@@ -3,6 +3,8 @@ extends StaticBody2D
 onready var derecha = get_node("derecha")
 onready var camara = utils.get_nodo_principal().get_node("camara")
 
+signal exit_tree
+
 func _ready():
 	add_to_group(juego.GRUPO_CANOS)
 	pass
@@ -12,4 +14,5 @@ func _process(delta):
 	
 	if derecha.get_global_position().x <= camara.get_total_pos().x:
 		queue_free()
+		emit_signal("exit_tree")
 	pass
